41bdf1e24cdb33cfb66e11b8e426ad3f131b0a4f
a30768 <a30768@alunos.ipca.pt>
2024-11-04 19:34:03 +0000

    Add files via upload

---
+++ SC_04-11-2024.py
@@ -0,0 +1,4 @@
+str1=input('Informe por favor o seu nome cliente:')
+str2=input('Digite por favor o nome da fita:')
+n1=int(input('Qual a quantidade que pretende cortar em kg?'))
+print('O seu nome é {} e pretende cortar {} kg do material {} '.format(str1, n1, str2))
